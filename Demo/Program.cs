8da58307ab9618c544a2e31abe9eed17ed881331
AhmedZhran404 <zxcvbnmahmed13579@gmail.com>
2025-06-29 18:28:06 +0300

    Casting Implicit And Explicit And Methods v3->v5

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -35,6 +35,164 @@ namespace Demo
             double number = 12345.67;
             Console.WriteLine(number.ToString("N", ArabicCulture));
             #endregion
+
+            #region Casting
+                #region ImplicitAndExplicit
+            /*
+                 Casting: converting a value from one type to another.
+                 There are two types of casting in C#:
+
+                 1. Implicit Casting:
+                    - Happens automatically.
+                    - From a smaller type to a larger type (e.g., int to float).
+                    - No risk of data loss.
+
+                 2. Explicit Casting:
+                    - Requires manual casting using syntax like (int).
+                    - From a larger type to a smaller type or between incompatible types.
+                    - May cause data loss or runtime errors.
+             */
+
+            int num1 = 1000; // 4 Bytes
+
+            long num2 = num1; // 8 Bytes  -> Implicit casting
+
+            Console.WriteLine(num2);
+            Console.WriteLine("---------------------");
+
+            long S1 = 1000;
+
+            // int s2 = S1; // Can not Implicit Convert Invaild type related error
+
+            long S2 = (int)S1; // Explicit Convert
+
+            // If the value of S1 is too large to fit in an int,
+            // it will cause data loss (overflow or truncated value).
+
+            Console.WriteLine("-------------------------");
+
+            //checked
+            //{
+            //long s4 = 547334946739464;
+
+            //int s5 = (int)s4;
+
+            //Console.WriteLine(s5);
+
+            //}
+
+            unchecked
+            {
+                long s4 = 547334;
+
+                int s5 = (int)s4;
+
+                Console.WriteLine(s5);
+            }
+
+            long number01 = 46758;
+            if (number01 > int.MaxValue || number01 < int.MinValue)
+            {
+                Console.WriteLine("Exception Will be Occuer");
+            }
+            else
+            {
+                int number02 = (int)number01;
+                Console.WriteLine(number02);
+            }
+
+            #endregion
+
+            #region MethodsOFCasting
+                /*
+                            #region Convert
+                            Console.WriteLine("Please Enter Your Name: ");
+                            string? name = Console.ReadLine(); // Read input From User by console
+                            Console.WriteLine("------------------------");
+                            Console.WriteLine("Pleasse Enter Your Age");
+                            // int age  = Console.ReadLine(); // Error ReadLine -> Always Take Input String
+                            // int age = (int)Console.ReadLine(); // Explicit Casting -> Invalid
+                            int age = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("------------------------");
+                            Console.WriteLine("Pleasse Enter Your Salary");
+                            decimal Salary = Convert.ToDecimal(Console.ReadLine());
+                            Console.WriteLine("------------------------");
+
+                            Console.Clear();
+                            Console.WriteLine("Employee Date :");
+                            Console.WriteLine($"Name is: {name}");
+                            Console.WriteLine($"Age is: {age}");
+                            Console.WriteLine($"Salary is: {Salary}");
+
+                            #endregion
+                */
+
+                /*
+                #region Parse
+                Console.WriteLine("Please Enter Your Name: ");
+                string? name1 = Console.ReadLine(); // Read input From User by console
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Pleasse Enter Your Age");
+                // int age  = Console.ReadLine(); // Error ReadLine -> Always Take Input String
+                // int age = (int)Console.ReadLine(); // Explicit Casting -> Invalid
+                int age1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Pleasse Enter Your Salary");
+                decimal Salary1 = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("------------------------");
+
+                Console.Clear();
+                Console.WriteLine("Employee Date :");
+                Console.WriteLine($"Name is: {name1}");
+                Console.WriteLine($"Age is: {age1}");
+                Console.WriteLine($"Salary is: {Salary1}");
+
+                #endregion
+                */
+
+                #region TryParse
+                    // string number04 = "100";
+                    // bool isParse1 = int.TryParse(number04, out int x01);
+                    // Console.WriteLine("isParse = " + isParse1 + " And X01 = " + x01);
+
+                    //Console.WriteLine("-------------------------------");
+                    //string number03 = "0";
+                    // bool isParse2 = int.TryParse(number03, out int x02);
+                    
+
+                    //if(isParse2)
+                    //{
+                    // Console.WriteLine("Converted and zero in x from number03"); ;
+                    //}else
+                    //{
+                    //   Console.WriteLine("Zero in x beacuse can not convert");
+                    //}
+
+            #region ExampleTryParse
+            Console.WriteLine("Please Enter Your Name: ");
+            string? name02 = Console.ReadLine(); // Read input From User by console
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Pleasse Enter Your Age");
+            // int age  = Console.ReadLine(); // Error ReadLine -> Always Take Input String
+            // int age = (int)Console.ReadLine(); // Explicit Casting -> Invalid
+            int.TryParse(Console.ReadLine(), out int age02);
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Pleasse Enter Your Salary");
+            decimal.TryParse(Console.ReadLine(), out decimal Salary02);
+            Console.WriteLine("------------------------");
+
+            Console.Clear();
+            Console.WriteLine("Employee Date :");
+            Console.WriteLine($"Name is: {name02}");
+            Console.WriteLine($"Age is: {age02}");
+            Console.WriteLine($"Salary is: {Salary02}");
+
+            #endregion
+
+            #endregion
+            #endregion
+            #endregion
+
         }
     }
 }
