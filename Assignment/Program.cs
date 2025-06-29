9b61a5cd3dfd9030efe969219ff9852806975e53
AhmedZhran404 <zxcvbnmahmed13579@gmail.com>
2025-06-29 21:26:59 +0300

    first commit

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,18 +4,8 @@
     {
         static void Main(string[] args)
         {
-            #region FractionsAndDiscard
-
-            float x = 3.5677777777777f;
-            double y = 4.5; // default num is double no problem
-
-            decimal z = 5.666m;
-
-            Console.WriteLine(x);
-
-            #endregion
-
-
+            Console.WriteLine("Welcome");
+           
         }
     }
 }
