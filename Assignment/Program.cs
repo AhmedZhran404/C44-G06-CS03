226d4cb49173458951c9b756c0f015d866cc8bf9
AhmedZhran404 <zxcvbnmahmed13579@gmail.com>
2025-06-29 22:13:47 +0300

    commit all

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,30 +5,30 @@
         static void Main(string[] args)
         {
 
-           // #region Question1
-           // /*
-           //  Write a program that allows the user to enter a number then print it.
-           // */
-           // Console.WriteLine("Enter Your Number: ");
-           // int.TryParse(Console.ReadLine(), out int number);
-           // Console.WriteLine($"The Number is: {number}");
+            #region Question1
+            /*
+             Write a program that allows the user to enter a number then print it.
+            */
+            Console.WriteLine("Enter Your Number: ");
+            int.TryParse(Console.ReadLine(), out int number);
+            Console.WriteLine($"The Number is: {number}");
 
-           // #endregion
+            #endregion
 
-           // #region Question2
-           // /*
-           //      Write C# program that converts a string to an 
-           //       integer, but the string contains non-numeric 
-           //       characters. And mention what will happen 
-           //*/
+            #region Question2
+            /*
+                 Write C# program that converts a string to an 
+                  integer, but the string contains non-numeric 
+                  characters. And mention what will happen 
+           */
 
-           // string name = "ahmed";
-           // // int num = (int)name; // Error can not conver
+            string name = "ahmed";
+            // int num = (int)name; // Error can not conver
 
-           // // int num = Convert.ToInt32(name); // Exception
+            // int num = Convert.ToInt32(name); // Exception
 
-           // // All conversion methods will throw an exception except TryParse, which handles the conversion safely.
-           // #endregion
+            // All conversion methods will throw an exception except TryParse, which handles the conversion safely.
+            #endregion
 
             #region Question3
             /*
